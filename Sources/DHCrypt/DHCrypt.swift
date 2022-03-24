d46d9c1aaa256741dd6a9baf1f98581affd7c137
Dan Hart <dhart94@gmail.com>
2022-03-23 20:00:11 -0400

    Add more API coverage

--- Sources/DHCrypt/DHCrypt.swift
+++ Sources/DHCrypt/DHCrypt.swift
@@ -2,48 +2,114 @@ import Foundation
 import FileKit
 
 open class DHCrypt: DHCrypting {
-    public static let subfolderName = "Sourceless"
+    public static var shared: DHCrypting = DHCrypt()
+    
+    public static let subfolderName = "DHCrypt"
     public static let dataFileExtension = "json"
     
-    public var shard: DHCrypting = DHCrypt()
-    
-    // MARK: - Methods
-    
-    /// Add keys, then use this method to retrieve them at runtime.
-    /// - Parameter fromKey: the identifier for the value needed
-    /// - Returns: the value from disk for the specified key
-    public func getValue(fromKey: String) throws -> String? {
+    public var cryptPath: Path {
         guard let documentsPath = FileManager.default.documentsDirectoryPath else {
-            throw DHCryptError.invalidDocumentsPath
+            return Path.current
         }
-        
-        let searchPath = documentsPath + Path(DHCrypt.subfolderName)
-        
-        let jsonFiles = searchPath.find(searchDepth: 1) { path in
+        return documentsPath + Path(DHCrypt.subfolderName)
+    }
+    
+    public var jsonFilePathArray: [Path] {
+        let jsonFiles = cryptPath.find(searchDepth: 1) { path in
             path.pathExtension == DHCrypt.dataFileExtension
         }
         
         if jsonFiles.isEmpty {
-            throw DHCryptError.emptyDirectoryNoJSONFiles
+            return []
+        } else {
+            return jsonFiles
         }
-        
-        for jsonFilePath in jsonFiles {
-            let codableFile = File<CryptHexStore>(path: jsonFilePath)
+    }
+    
+    public var codableFiles: [File<CryptHexStore>] {
+        var codableFiles = [File<CryptHexStore>]()
+        for jsonFilePath in jsonFilePathArray {
+            codableFiles.append(File<CryptHexStore>(path: jsonFilePath))
+        }
+        return codableFiles
+    }
+    
+    public var keyValuePairArray: [[String: String]] {
+        var keyValuePairArray: [[String: String]] = []
+        for codableFile in codableFiles {
             guard let store = try? codableFile.read() else { continue }
             guard let decrypted = store.decrypted else { continue }
-            guard let keyValuePairs = try? convert(fromData: decrypted) else { continue }
+            if let keyValuePair = try? convert(fromData: decrypted) {
+                keyValuePairArray.append(keyValuePair)
+            }
+        }
+        return keyValuePairArray
+    }
+    
+    public var keyValuePairs: [String: String]? {
+        var keyValuePairsDictionary: [String: String] = [:]
+        for keyValuePairs in keyValuePairArray {
             for keyValuePair in keyValuePairs {
-                if keyValuePair.key == fromKey {
-                    return keyValuePair.value
+                let key = keyValuePair.key
+                let value = keyValuePair.value
+                if let _ = keyValuePairsDictionary[key] {
+                    return nil
+                } else {
+                    keyValuePairsDictionary[key] = value
                 }
+            }
+        }
+        return keyValuePairsDictionary
+    }
+    
+    // MARK: - Methods
+    
+    public func encrypt(stringDictionary: [String: String]) throws -> Path? {
+        return try encrypt(stringDictionary.data)
+    }
+    
+    public func encrypt(_ data: Data?) throws -> Path? {
+        guard let data = data else {
+            return nil
+        }
+
+        let store = data.encrypt()
+        return try store?.writeToDisk(at: cryptPath)
+    }
+    
+    /// Add keys, then use this method to retrieve them at runtime.
+    /// - Parameter fromKey: the identifier for the value needed
+    /// - Returns: the value from disk for the specified key
+    public func decryptValue(fromKey: String) throws -> String? {
+        guard let keyValuePairs = keyValuePairs else {
+            return nil
+        }
+        
+        for keyValuePair in keyValuePairs {
+            if keyValuePair.key == fromKey {
+                return keyValuePair.value
             }
         }
         
         return nil
     }
     
+    public func delete(key: String) throws -> Path? {
+        for codableFile in codableFiles {
+            guard let store = try? codableFile.read() else { continue }
+            guard let decrypted = store.decrypted else { continue }
+            if let keyValuePairs = try? convert(fromData: decrypted) {
+                for keyValuePair in keyValuePairs where keyValuePair.key == key {
+                    try codableFile.delete()
+                    return codableFile.path
+                }
+            }
+        }
+        return nil
+    }
+    
     // MARK: - Helper
-    func convert(fromData: Data) throws -> [String: String]? {
+    public func convert(fromData: Data) throws -> [String: String]? {
         return try JSONSerialization.jsonObject(with: fromData, options: []) as? [String: String]
     }
 }
--- Sources/DHCrypt/DHCrypting.swift
+++ Sources/DHCrypt/DHCrypting.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import FileKit
 
 public protocol DHCrypting {
+    static var shared: DHCrypting { get set }
     
+    func encrypt(stringDictionary: [String: String]) throws -> Path?
+    func encrypt(_ data: Data?) throws -> Path?
+    func decryptValue(fromKey: String) throws -> String?
+    func delete(key: String) throws -> Path?
+    func convert(fromData: Data) throws -> [String: String]?
 }
